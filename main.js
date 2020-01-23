84fa82e9b07a02fd1455ba4a45140cea70f1f2d0
s0ick <36438226+s0ick@users.noreply.github.com>
2020-01-23 16:17:10 +0300

    fix: variable declaration in jsy

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-var money = 49;
-var income = 70;
-var addExpenses = 10;
-var deposit = 100;
-var mission = 100;
-var period = 5;
+let money = 49, income = 70, addExpenses = 10, deposit = 100, mission = 100, period = 5;
 
 alert("Переменные добавлены!");
 console.log("Привет, куратор :D");
