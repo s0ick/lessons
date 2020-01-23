b380accc3a908ed104f89e59809d5bb133e9c96c
s0ick <36438226+s0ick@users.noreply.github.com>
2020-01-23 11:18:15 +0300

    Первый коммит

---
+++ main.js
@@ -0,0 +1,9 @@
+var money = 49;
+var income = 70;
+var addExpenses = 10;
+var deposit = 100;
+var mission = 100;
+var period = 5;
+
+alert("Переменные добавлены!");
+console.log("Привет, куратор :D");
