7013be96254a1879fa0a4cce284340661b57f84d
s0ick <36438226+s0ick@users.noreply.github.com>
2020-01-24 13:23:01 +0300

    second point | js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 let num = 266219;
+let size = 0;
+let composition = 1;
+
+size = String(num).length;
+
+for(let i = 0; i < size; i++) {
+  composition *= (num % 10);
+  num = Math.trunc(num / 10);
+}
+console.log(composition);
+
+
