4b51d44b697bca48e192fc4af473b76e2d923d8a
s0ick <36438226+s0ick@users.noreply.github.com>
2020-01-24 12:43:53 +0300

    second point | js

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let money = 15000,
 income = 'фриланс',
-addExpenses = 'интернет, проезд, еда',
+addExpenses = 'Интернет, Проезд, Еда',
 deposit = true,
 mission = 30000,
 period = 5;
 
 console.log(typeof(money));
 console.log(typeof(income));
-console.log(typeof(deposit));
+console.log(typeof(deposit) + '\n');
 
+console.log(addExpenses.length + '\n');
+
+console.log('Период равен ' + period + ' месяцев.' + ' Цель заработать ' + mission + ' рублей' + '\n');
+
+console.log(addExpenses.toLowerCase().split(', ') + '\n');
+
+let budgetDay = money / 30;
+console.log(budgetDay);
