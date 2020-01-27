86d7412448ab163604bcf046729de128b92532ea
s0ick <36438226+s0ick@users.noreply.github.com>
2020-01-27 18:25:58 +0300

    fix: new array | js

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 'use strict';
 
 let lang = prompt('Укажите язык', 'ru');
-let arr = [
-  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  ['Monday', 'Tuesday', 'Wednesday', 'Thursday ', 'Friday', 'Saturday', 'Sunday']
-];
+let obj = {
+  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday ', 'Friday', 'Saturday', 'Sunday']
+};
 
-if (lang === 'ru') {
-  console.log('Понедельник \nВторник \nСреда \nЧетверг \nПятница \nСуббота \nВоскресенье');
-} else {
-  console.log('Monday \nTuesday \nWednesday \nThursday \nFriday \nSaturday \nSunday');
-}
+ if (lang === 'ru') {
+   console.log('Понедельник \nВторник \nСреда \nЧетверг \nПятница \nСуббота \nВоскресенье');
+ } else {
+   console.log('Monday \nTuesday \nWednesday \nThursday \nFriday \nSaturday \nSunday');
+ }
 
-switch(lang) {
-  case 'ru': console.log('Понедельник \nВторник \nСреда \nЧетверг \nПятница \nСуббота \nВоскресенье');
-             break;
-  case 'en': console.log('Monday \nTuesday \nWednesday \nThursday \nFriday \nSaturday \nSunday');
-             break;          
-}
+ switch(lang) {
+   case 'ru': console.log('Понедельник \nВторник \nСреда \nЧетверг \nПятница \nСуббота \nВоскресенье');
+              break;
+   case 'en': console.log('Monday \nTuesday \nWednesday \nThursday \nFriday \nSaturday \nSunday');
+              break;          
+ }
 
-console.log(lang === 'ru' ? arr[0] : arr[1]);
+console.log(obj[lang]);
 
 let namePerson = prompt('Ваше имя');
 console.log(namePerson === 'Артём' ? 'Директор' : namePerson === 'Максим' ? 'Преподаватель' : 'Студент');
