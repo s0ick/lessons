41dc3c050496ea1a9d453ae8d939eacbfe0d565f
s0ick <36438226+s0ick@users.noreply.github.com>
2020-01-27 17:19:09 +0300

    Point 1 | js

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let lang = prompt('Укажите язык', 'ru');
+let arr = [
+  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday ', 'Friday', 'Saturday', 'Sunday']
+];
+
+if (lang === 'ru') {
+  console.log('Понедельник \nВторник \nСреда \nЧетверг \nПятница \nСуббота \nВоскресенье');
+} else {
+  console.log('Monday \nTuesday \nWednesday \nThursday \nFriday \nSaturday \nSunday');
+}
+
+switch(lang) {
+  case 'ru': console.log('Понедельник \nВторник \nСреда \nЧетверг \nПятница \nСуббота \nВоскресенье');
+             break;
+  case 'en': console.log('Monday \nTuesday \nWednesday \nThursday \nFriday \nSaturday \nSunday');
+             break;          
+}
+
+console.log(lang === 'ru' ? arr[0] : arr[1]);
+
