e9147b99eeea904895bbd1a8d606767366636491
s0ick <36438226+s0ick@users.noreply.github.com>
2020-01-27 13:56:44 +0300

    feature: point 1-5 | js

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-let money = 15000,
+'use strict';
+
+let money = prompt('Ваш месячный доход?', '15000'),
 income = 'фриланс',
-addExpenses = 'Интернет, Проезд, Еда',
-deposit = true,
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 
+ 'Интернет, Проезд, Еда'),
+deposit = confirm('Есть ли у вас депозит в банке?'),
 mission = 30000,
 period = 5;
+
+let expenses1 = prompt('Введите обязательную статью расходов?'),
+amount1 = prompt('Во сколько это обойдется?'),
+expenses2 = prompt('Введите обязательную статью расходов?'),
+amount2 = prompt('Во сколько это обойдется?');
+
 
 console.log(typeof(money));
 console.log(typeof(income));
 console.log(typeof(deposit));
 
 console.log(addExpenses.length);
-
-console.log('Период равен ${period} месяцев. Цель заработать ${mission} рублей');
+console.log(`Период равен ${period} месяцев. Цель заработать ${mission} рублей`);
 
 console.log(addExpenses.toLowerCase().split(', '));
 
