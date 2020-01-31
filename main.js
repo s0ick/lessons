a1332d63aedabf896f21bbfc032f3575002720c2
s0ick <36438226+s0ick@users.noreply.github.com>
2020-02-01 01:00:38 +0300

    feature: new style & full task | css&js

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 'use strict';
 
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let day = new Date();
 
-week.forEach(function(item){
-  document.write(`<p class="list">${item}</p>`);
+week.forEach(function(item, index){
+  if (index === (day.getDay() - 1)) {
+    document.write(`<p class="list bold"><b>${item}</b></p>`);
+  } else if(item === 'Суббота' || item === 'Воскресенье'){
+    document.write(`<p class="list"><i>${item}</i></p>`);
+  }  else {
+    document.write(`<p class="list">${item}</p>`);
+  }
 });
