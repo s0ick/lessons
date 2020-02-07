e987bcb0c370c8cbede77d78e1113cf45b66fcb5
s0ick <36438226+s0ick@users.noreply.github.com>
2020-02-07 15:40:50 +0300

    fix: обе функции | js

--- main.js
+++ main.js
@@ -1,53 +1,40 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-      month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля',
-               'Августа', 'Сентября', 'Ноября', 'Декабря'];
+let decCache = [],
+      decCases = [2, 0, 1, 1, 1, 2]; 
+
+function declOfNum(number, titles)  
+  {  
+    if(!decCache[number]) decCache[number] = number % 100 > 4 && number % 100 < 20 ? 2 : decCases[Math.min(number % 10, 5)];
+    return titles[decCache[number]];
+  }
 
 function formatDateOne(date) {
-  let str = 'Сегодня';
+  let str = 'Сегодня ';
 
-  week.forEach(function(item, index){
-    if (index === (date.getDay() - 1)) {
-      str = str + ' ' + item;
-    }
-  });
-
-  str = str + ', ' + date.getDay();
-
-  month.forEach(function(item, index){
-    if (index === (date.getMonth() - 1)) {
-      str = str + ' ' + item;
-    }
-  });
-
-  str = str + ' ' + date.getFullYear() + ' года, ';
-  if(date.getHours() === 1 || date.getHours() === 21) {
-    str = str + date.getHours() + ' час';
-  } else if((date.getHours() >= 2 && date.getHours() <= 4) || date.getHours() === 22 || date.getHours() === 23) {
-    str = str + date.getHours() + ' часа';
-  } else if(date.getHours() >= 5 && date.getHours() <= 20) {
-    str = str + date.getHours() + ' часов';
-  } else if(date.getHours() === 0) {
-    str = str + ' полночь';
-  }
-  return `${str} ${date.getMinutes()} минут ${date.getSeconds()} секунд`;
+  let options = { weekday: 'long'};
+  str = str + date.toLocaleString('ru', options);
+  str = str.slice(0,7) + ' ' + str.split(' ')[1].toUpperCase().slice(0,1) + str.slice(9) + ', ';
+  options = {day: 'numeric', month: 'long', year: 'numeric'};
+  str = str + date.toLocaleString('ru', options);
+  str = str.substring(0, (str.length -1)) + 'ода, ';
+  options = {hour: 'numeric'};
+  str = str + date.toLocaleString('ru', options) + ' ' + declOfNum(date.toLocaleString('ru', options),['час', 'часа', 'часов']);
+  options = {minute: 'numeric'};
+  str = str + ' ' + date.toLocaleString('ru', options) + ' минут ';
+  options = {second:'numeric'};
+  str = str + ' ' + date.toLocaleString('ru', options) + ' секунды';
+  return str;
 }
 
-function formatDateTwo(date) {
-  let dd = date.getDate();
-  if (dd < 10) dd = '0' + dd;
-  let mm = date.getMonth() + 1;
-  if (mm < 10) mm = '0' + mm;
-  let yy = date.getFullYear() % 100;
-  if (yy < 10) yy = '0' + yy;
-  let h = date.getHours();
-  if (h < 10) h = '0' + h;
-  let m = date.getMinutes();
-  if (m < 10) m = '0' + m;
-  let s = date.getSeconds();
-  if (s < 10) s = '0' + s;
-  return `${dd}.${mm}.${yy} - ${h}:${m}:${s}`;
+function formatDateTwo(dd, mm, yy, h, m, s) {
+  function TF(temp) {
+    if(temp < 10) {
+      temp = '0' + temp;
+    }
+    return temp;
+  }
+  return `${TF(dd)}.${TF(mm)}.${TF(yy)} - ${TF(h)}:${TF(m)}:${TF(s)}`;
 }
 
 let pStr = document.createElement('p'),
@@ -60,5 +47,6 @@ document.body.appendChild(pNum);
 setInterval(function(){
   let day = new Date();
   pStr.textContent = formatDateOne(day);
-  pNum.textContent = formatDateTwo(day);
+  pNum.textContent = formatDateTwo(day.getDate(), day.getMonth() + 1, day.getFullYear() % 100,
+                                   day.getHours(), day.getMinutes(), day.getSeconds());
 }, 1000);
