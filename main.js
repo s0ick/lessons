9f6f408e7fcf9faee480fbc929bde995b003318e
s0ick <36438226+s0ick@users.noreply.github.com>
2020-02-13 23:56:51 +0300

    feature: full task 16_2 | js

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 'use strict';
 
+class First {
+  hello() {
+    console.log('Привет я метод родителя!');
+  }
+}
+class Second extends First {
+  hello(){
+    super.hello(); 
+    console.log('А я наследуемый метод!');
+  }
+}
+const tmp = new Second();
+tmp.hello();
